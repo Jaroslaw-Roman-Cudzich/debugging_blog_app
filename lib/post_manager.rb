12553db5e9df66ba19e5d220a21cac5d06f4113c
Jaroslaw Roman Cudzich <internet.jarek@gmail.com>
2022-02-11 13:32:13 +0000

    Corrected code to meet the user's requirements including displaying posts for a specific tag in response to the RSpec error messages.

---
+++ lib/post_manager.rb
@@ -0,0 +1,25 @@
+class PostManager
+  def self.instance
+    @instance ||= self.new
+  end
+
+  def initialize
+    @posts = []
+  end
+
+  def add_post(post)
+    @posts << post
+  end
+
+  def all_posts
+    @posts
+  end
+
+  def all_posts_by_tag(tag)
+    @posts.select do |post|
+      post.tags.include?(tag)
+      # p "------here------"
+      # p @posts
+    end
+  end
+end
